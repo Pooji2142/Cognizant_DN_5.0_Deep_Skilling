46e1dc25cab727db99bb91f70530fe992bb87275
Pooji2142 <poojithamuvvala31@gmail.com>
2026-06-24 21:57:39 +0530

    Add JUnit basic testing exercises

---
+++ WEEK-1/04_JUnit_Basic_Testing/JUnitAAAExample/src/test/java/CalculatorTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setup Method Executed");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown Method Executed");
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @Test
+    public void testMultiplication() {
+
+        // Arrange
+        int a = 10;
+        int b = 5;
+
+        // Act
+        int result = calculator.multiply(a, b);
+
+        // Assert
+        assertEquals(50, result);
+    }
+}
