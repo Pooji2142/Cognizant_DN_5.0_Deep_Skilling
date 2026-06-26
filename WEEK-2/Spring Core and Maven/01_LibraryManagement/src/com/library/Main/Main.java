94f731d87b559397f076db2652dfadcdccba7b4c
Pooji2142 <poojithamuvvala31@gmail.com>
2026-06-26 16:54:01 +0530

    Add Spring Core XML configuration for LibraryManagement

---
+++ WEEK-2/Spring Core and Maven/01_LibraryManagement/src/com/library/Main/Main.java
@@ -0,0 +1,22 @@
+package com.library.Main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service = (BookService) context.getBean("bookService");
+        service.displayService();
+
+        BookRepository repository = (BookRepository) context.getBean("bookRepository");
+        repository.displayRepository();
+    }
+}
