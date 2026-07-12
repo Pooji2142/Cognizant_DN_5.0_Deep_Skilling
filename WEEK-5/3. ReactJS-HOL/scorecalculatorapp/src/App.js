05f8561ab08994537bfe69d820e26c09c8817274
Pooji2142 <poojithamuvvala31@gmail.com>
2026-07-12 19:10:25 +0530

    Added Week 5

---
+++ WEEK-5/3. ReactJS-HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div>
+      <CalculateScore
+        Name={"Poojitha"}
+        School={"ABC Public School"}
+        total={284}
+        goal={3}
+      />
+    </div>
+  );
+}
+
+export default App;
